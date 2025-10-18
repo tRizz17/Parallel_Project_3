560e0710a76150630f3ce8c6b99b3c1b67e49748
Thomas Rizzo <rizzot@flip2.engr.oregonstate.edu>
2025-10-17 20:32:15 -0700

    Moved project to flip, changed make, script files

--- Makefile
+++ Makefile
@@ -1,2 +1,7 @@
+# FOR MAC
+# proj03:	proj03.cpp
+# 		clang++ -Wall -Wextra -Werror -Wno-sign-compare -Wno-unused-parameter -Wno-format -Wno-missing-field-initializers -O0 -Xpreprocessor -fopenmp -I/opt/homebrew/opt/libomp/include -lm -L/opt/homebrew/opt/libomp/lib -lomp proj03.cpp -o proj03	
+
+
 proj03:	proj03.cpp
-		clang++ -Wall -Wextra -Werror -Wno-sign-compare -Wno-unused-parameter -Wno-format -Wno-missing-field-initializers -O0 -Xpreprocessor -fopenmp -I/opt/homebrew/opt/libomp/include -lm -L/opt/homebrew/opt/libomp/lib -lomp proj03.cpp -o proj03	
+		g++	proj03.cpp	-o	proj03	-lm	-fopenmp
--- script.sh
+++ script.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
+
+# FOR MAC
+# for t in 1 2 4 6 8
+# do
+#   for n in 2 3 4 5 10 15 20 30 40 50
+#   do
+#     clang++ -DNUMT=$t -DNUMCAPITALS=$n -Wall -Wextra -Werror -Wno-sign-compare -Wno-unused-parameter -Wno-format -Wno-missing-field-initializers -O0 -Xpreprocessor -fopenmp -I/opt/homebrew/opt/libomp/include -lm -L/opt/homebrew/opt/libomp/lib -lomp proj03.cpp -o proj03
+#     ./proj03
+#   done
+# done
+
+
+
+#!/bin/bash
 for t in 1 2 4 6 8
 do
   for n in 2 3 4 5 10 15 20 30 40 50
   do
-    clang++ -DNUMT=$t -DNUMCAPITALS=$n -Wall -Wextra -Werror -Wno-sign-compare -Wno-unused-parameter -Wno-format -Wno-missing-field-initializers -O0 -Xpreprocessor -fopenmp -I/opt/homebrew/opt/libomp/include -lm -L/opt/homebrew/opt/libomp/lib -lomp proj03.cpp -o proj03
+     g++   proj03.cpp  -DNUMT=$t -DNUMCAPITALS=$n  -o proj03  -lm  -fopenmp
     ./proj03
   done
 done
